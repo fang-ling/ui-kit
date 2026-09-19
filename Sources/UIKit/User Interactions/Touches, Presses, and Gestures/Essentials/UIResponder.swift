2cad39eda846f54a64effb70fda7e1cfdec33601
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2026-09-19 21:27:55 +0800

    Responder: Update documentation

--- Sources/UIKit/User Interactions/Touches, Presses, and Gestures/Essentials/UIResponder.swift
+++ Sources/UIKit/User Interactions/Touches, Presses, and Gestures/Essentials/UIResponder.swift
@@ -40,14 +40,12 @@
 /// - ``next``
 @MainActor
 open class UIResponder {
-  /// Returns the next responder in the responder chain, or `nil` if there's no next responder.
+  /// The next responder in the responder chain, or `nil` if there's no next responder.
   ///
   /// The ``UIResponder`` class doesn't store or set the next responder automatically, so this method returns `nil` by default. Subclasses must override this method and return an appropriate next
   /// responder. For example, ``UIView`` implements this method and returns the ``UIViewController`` object that manages it (if it has one) or its superview (if it doesn't). ``UIViewController``
   /// similarly implements the method and returns its view's superview. ``UIWindow`` returns the application object. The shared ``UIApplication`` object normally returns `nil`, but it returns its app
   /// delegate if that object is a subclass of ``UIResponder`` and hasn't already been called to handle the event.
-  ///
-  /// - Returns: The next object in the responder chain, or `nil` if this is the last object in the chain.
   open var next: UIResponder? {
     return nil
   }
